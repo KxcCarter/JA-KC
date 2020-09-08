becae94c12edd0dd4035e5f4e737d488b09662a7
Ailea Patrinely <aileapatrinely@gmail.com>
2020-09-08 10:40:03 -0500

    adds report-form route to server

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import sessionMiddleware from './modules/session-middleware';
 import passport from './strategies/user.strategy';
 import userRouter from './routes/user.router';
 import trainingRouter from './routes/training.router';
+import reportformRouter from './routes/report-form.router';
 
 require('dotenv').config();
 
@@ -23,6 +24,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/training', trainingRouter);
+app.use('/api/report-form', reportformRouter);
 
 // Serve static files
 app.use(express.static('build'));
