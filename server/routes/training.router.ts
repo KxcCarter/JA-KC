815b8c2766cc741d1063c7554ed3316fb7d033f5
Ailea Patrinely <aileapatrinely@gmail.com>
2020-09-08 09:44:26 -0500

    creates training GET route, needs queryText

--- server/routes/training.router.ts
+++ server/routes/training.router.ts
@@ -10,7 +10,11 @@ const router: express.Router = express.Router();
 router.get(
   '/',
   (req: Request, res: Response, next: express.NextFunction): void => {
-    // GET route code here
+    const queryText = `SELECT`;
+    pool
+      .query(queryText, [req.body])
+      .then((response) => res.send(response.rows))
+      .catch((error) => console.log('Error in training GET:', error));
   }
 );
 
