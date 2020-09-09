156a3aa8bf6778ebca94629b2cc6d87937684661
Ailea Patrinely <aileapatrinely@gmail.com>
2020-09-09 15:08:40 -0500

    adds delete and edit routes to training router

--- server/routes/training.router.ts
+++ server/routes/training.router.ts
@@ -36,6 +36,34 @@ router.post(
   }
 );
 
-router.delete('/:id');
+router.delete(
+  '/:id',
+  (req: Request, res: Response, next: express.NextFunction): void => {
+    const id: number = req.params.id;
+    const queryText = `DELETE FROM "learning_material" WHERE "learning_material".id=$1`;
+    pool
+      .query(queryText, [id])
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  }
+);
+
+router.put(
+  '/:id',
+  (req: Request, res: Response, next: express.NextFunction): void => {
+    const program_id: string = req.body.program_id;
+    const title: string = req.body.title;
+    const content: string = req.body.content;
+
+    const queryText = `UPDATE "learning_material" SET "program_id"=$1, "title"=$2, "content"=$3);`;
+    pool
+      .query(queryText, [program_id, title, content])
+      .then(() => res.sendStatus(201))
+      .catch(() => res.sendStatus(500));
+  }
+);
 
 export default router;
