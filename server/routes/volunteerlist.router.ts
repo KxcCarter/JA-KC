774cff2c2494c1f7714bf79a9fea19a4e13df25e
Ailea Patrinely <aileapatrinely@gmail.com>
2020-09-11 09:45:49 -0500

    creates edit and delete routes and sagas for volunteerlist

--- server/routes/volunteerlist.router.ts
+++ server/routes/volunteerlist.router.ts
@@ -19,4 +19,35 @@ router.get(
   }
 );
 
+router.delete(
+  '/:id',
+  (req: Request, res: Response, next: express.NextFunction): void => {
+    const id: number = req.params.id;
+    const queryText = `DELETE FROM "users" WHERE "users".id=$1`;
+    pool
+      .query(queryText, [id])
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  }
+);
+
+router.put('/:id', (req, res) => {
+  const id: number = req.params.id;
+  const username: string = <string>req.body.username;
+  const first_name: string = req.body.first_name;
+  const last_name: string = req.body.last_name;
+  const email: string = req.body.email;
+  const telephone: string = req.body.telephone;
+
+  const queryText =
+    'UPDATE "users" SET "username"=$1, "first_name"=$2, "last_name"=$3, "email"=$4, "telephone"=$5 WHERE "users"=$6;';
+  pool
+    .query(queryText, [username, first_name, last_name, email, telephone, id])
+    .then(() => res.sendStatus(201))
+    .catch(() => res.sendStatus(500));
+});
+
 export default router;
