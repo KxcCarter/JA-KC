85f06fd3f1922c460d02b964b84d6bbec9ec9777
Ailea Patrinely <aileapatrinely@gmail.com>
2020-09-11 09:52:12 -0500

    adds sagas to _root.saga.js

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,6 +2,10 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
+import programSaga from './programs.saga';
+import editprogramSaga from './editprograms.saga';
+import deleteprogramSaga from './deleteprogram.saga';
+import deleteProgramSaga from './deleteprogram.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -15,5 +19,8 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
+    programSaga(),
+    editprogramSaga(),
+    deleteProgramSaga(),
   ]);
 }
