22aca43a3c1932bfa42c8e9875ac2f32c59a0e1c
Ailea Patrinely <aileapatrinely@gmail.com>
2020-09-17 11:34:06 -0500

    shows scheduled class list on userpage

--- src/components/UserPage/scheduledClasses.js
+++ src/components/UserPage/scheduledClasses.js
@@ -6,14 +6,13 @@ import { Paper, Box, Container } from '@material-ui/core';
 import './UserPage.css';
 import TestNav from '../MobileNav/TestNav';
 
-class UserPage extends Component {
+class scheduledClasses extends Component {
   componentDidMount() {
     this.props.dispatch({
       type: 'GET_SCHEDULED_CLASSES',
     });
   }
 
-  // this component doesn't do much to start, just renders some user info to the DOM
   render() {
     const scheduled_classes = this.props.store.volunteerScheduledClasses.map(
       (item, index) => {
@@ -37,30 +36,8 @@ class UserPage extends Component {
         );
       }
     );
-    return (
-      <div>
-        {scheduled_classes}
-
-        <Container>
-          <Paper className="ProgramResourcesBubbleStyle">
-            <Box p={1} m={1}>
-              {/* <p>Your scheduled classes are: {this.props.store.user.id}</p> */}
-              <h2>Program Resources</h2>
-            </Box>
-          </Paper>
-        </Container>
-        <Container>
-          <Paper className="ProgramResourcesBubbleStyle">
-            <Box p={1} m={1}>
-              <h2>Submit Class Details</h2>
-            </Box>
-          </Paper>
-        </Container>
-        {/* <LogOutButton className="log-in" /> */}
-      </div>
-    );
+    return <div>{scheduled_classes}</div>;
   }
 }
 
-// this allows us to use <App /> in index.js
-export default connect(mapStoreToProps)(UserPage);
+export default connect(mapStoreToProps)(scheduledClasses);
