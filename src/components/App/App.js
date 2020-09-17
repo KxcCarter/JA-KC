738fb0fc843f09fee762fdf2c8ee9528e0e53ed3
Skyler Burgard <skylerrburgard@hotmail.com>
2020-09-17 11:04:45 -0500

    Fixing capital letters?

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -36,14 +36,6 @@ class App extends Component {
 
             <Route exact path="/admin" component={AppDashboard} />
             <Route exact path="/adminvolunteers" component={AdminVolunteers} />
-<<<<<<< HEAD
-=======
-            <Route
-              exact
-              path="/adminadministrators"
-              component={AdminAdministrators}
-            />
->>>>>>> 7750e00dd88cc0d88fb24a932a9350945d5ce11a
             <Route exact path="/adminclasses" component={AdminClasses} />
             <Route exact path="/adminreports" component={AdminReports} />
             <Route exact path="/login" component={AdminLoginPage} />
