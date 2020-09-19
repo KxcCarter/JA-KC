6d2fe138e67f399e83a5fda9c9a65dc9ef87d057
David Walton <davidwalton6772@gmail.com>
2020-09-19 16:01:28 -0500

    updated add class modal table to scroll

--- src/components/content/AddClassModal.js
+++ src/components/content/AddClassModal.js
@@ -22,7 +22,8 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
     width: '60%',
-    height: '90%',
+    height: '100%',
+    overflow: 'auto',
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
